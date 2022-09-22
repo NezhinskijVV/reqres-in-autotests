26864108e85ded806e695217a9c373e6a5683485
YasiaN <yaroslavna.nezhinskaya@coins-global.com>
2022-09-22 18:23:40 +0400

    BUT-65 making test for Checking correctness of user's creation

--- src/test/java/ru/buttonone/YasiaMain.java
+++ src/test/java/ru/buttonone/YasiaMain.java
@@ -1,5 +1,27 @@
 package ru.buttonone;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+@DisplayName("API https://reqres.in/ должен")
 public class YasiaMain {
+
+    @DisplayName("корректно создавать пользователя")
+    @Test
+    public void shouldCreateUserCorrectly() {
+
+        given()
+                .baseUri("https://reqres.in")
+                .param("email","eve1.holt2@reqres.in")
+                .param("password","pistol1")
+                .when()
+                .get("/api/register")
+                .then()
+                .statusCode(200)
+        ;
+
+    }
 
 }
